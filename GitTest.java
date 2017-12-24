a27ac19a47d3d3c4c8ee53db78b984c58cc5335d
maturibayasi1025 <maturibayasi1025@gmail.com>
2017-12-24 13:34:11 +0900

    Add static .

--- GitTest.java
+++ GitTest.java
@@ -2,7 +2,7 @@ import java.lang.System;
 import java.util.Scanner;
 
 public class GitTest {
-    public void main(String args[]){
+    public static void main(String args[]){
 
         Scanner sc = new Scanner(System.in);
         
